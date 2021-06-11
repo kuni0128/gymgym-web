d52e8af7542524aea3c3dd374a8e692591d9a95f
kuniaki <k.ishikawa.0128@gmail.com>
2021-06-12 08:42:44 +0900

    feat: rails g rspec:install

--- api/app/controllers/trainee/v1/trainees/registrations_controller.rb
+++ api/app/controllers/trainee/v1/trainees/registrations_controller.rb
@@ -3,11 +3,12 @@
 module V1
   module Trainees
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      wrap_parameters format: []
 
       private
 
       def sign_up_params
-        params.permit(*params_for_resource(:sign_up))
+        params.permit(*params_for_resource(:sign_up), :confirm_success_url)
       end
     end
   end
